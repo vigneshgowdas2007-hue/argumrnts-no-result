43aa597cfaaac11424087233cb57348e8673af6b
vigneshgowdas2007-hue <vigneshgowdas2007@gmail.com>
2026-01-31 17:32:10 +0530

    Add files via upload

---
+++ star cat.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char s1[20]="good";
+char s2[]="morning";
+strcat (s1,s2);
+printf("result:%s",s1);
+}
---
+++ strcmp.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+char s1[]="apple";
+char s2[]="banana";
+int result=strcmp(s1,s2);
+printf("result :%d",result);
+
+}
